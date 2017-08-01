32f7f7c25d5ff590dff91a6a5f6e9d0fe4409c47
Camden Bickel <cam@bickel.io>
2017-08-01 10:44:34 -0500

    Added attacked system

---
+++ src/systems/AttackedSystem.js
@@ -0,0 +1,34 @@
+// @flow
+
+import { System } from 'systems/system';
+import type { Entity } from 'entity';
+import { Attacked } from 'component';
+import ComponentManager from 'component-manager';
+
+export default class AttackedSystem implements System {
+  componentManager: ComponentManager;
+  game: *;
+
+  constructor(componentManager: ComponentManager, game: *) {
+    this.componentManager = componentManager;
+    this.game = game;
+  }
+
+  update(entities: Array<Entity>) {
+    for (let myEntity of entities) {
+      const attackedComponent = this.componentManager.get({
+        entity: myEntity,
+        component: Attacked,
+      });
+
+      if (attackedComponent) {
+        console.log(`${myEntity} has been attacked! Ouch!`);
+        // Once attack is resolved, remove the "Attacked" component
+        this.componentManager.remove({
+          entity: myEntity,
+          component: Attacked,
+        });
+      }
+    }
+  }
+}
